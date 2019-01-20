00bfc2fd03e1bbbaf7f97ce71a630367c627ed62
imad-abdou <igentle.appletec@gmail.com>
2019-01-20 14:50:36 +0100

    add frappe style of read only and fix some bugs

--- eforms/api.py
+++ eforms/api.py
@@ -1,15 +1,13 @@
 import frappe
 
 @frappe.whitelist()
-def get_seen(dt,dn):
+def get_seen(dt, dn):
     df = frappe.db.get_value(dt, dn, "meta_seen")
     return df
+
 @frappe.whitelist()
-def set_seen():
-    df = frappe.db.set_value("eForm1", "EF001", "meta_seen", 1)
+def set_seen(dt, dn):
+    df = frappe.db.set_value(dt, dn, "meta_seen", 1)
     return "set"
-@frappe.whitelist()
-def get_ticket_status(dt, dn):
-    df = frappe.db.get_value(dt, dn, "status")
-    return df
+
 
--- eforms/eforms/web_form/eform1/eform1.js
+++ eforms/eforms/web_form/eform1/eform1.js
@@ -1,8 +1,8 @@
 //Note: to make any field read only (for the client side) you just add the attribute "disabled" with the vale of "1"
 // to the field element using js setAttribute an to set field to read and write just remove "disabled" attribute from the field element.
 
-// Add Here Any fields names which you want to keep tracking.
-const fields = ['supplier', 'contact', 'description']
+// add fields which you want to be read only based on status 
+const watchFields = ['supplier', 'contact', 'description']
 
 // check if submitted and show success message to the user.
 function isSubmitted() {
@@ -12,11 +12,13 @@ function isSubmitted() {
             args: {dt: web_form_settings.web_form_doctype, dn: web_form_settings.doc_name},
             callback: function(r) {
                if (r.message == 0) {
-                console.log("correct")
+                
                 // Edit the message as needed.
                 const success_title = 'Info Submitted Successfully.';
                 const success_msg = `<p>Your ticket ${web_form_settings.doc_name} have been submitted successfully.</p>
-                                     <p><a href=\"/\" class=\"btn btn-sm btn-default\">Done</a></p>`;
+                                         <p><a href=\"/${web_form_settings.web_form_name}\" 
+                                         class=\"btn btn-sm btn-default\">Back to ${web_form_settings.web_form_doctype}'s list</a>
+                                     </p>`;
                 frappe.msgprint(success_msg, success_title);
                 frappe.call({
                     method: "eforms.api.set_seen",
@@ -31,28 +33,22 @@ function isSubmitted() {
     }
 }
 function set_props() {
-    let sts = document.getElementById("sts");
-    
-    frappe.call({
-        method: "eforms.api.get_ticket_status",
-        args: {dt: web_form_settings.web_form_doctype, dn: web_form_settings.doc_name},
-        callback: function(r) {
-            let ticketStatus = r.message
-            // if save button clicked and the status is Completed then set fields as Read Only (Client side).
-            if(ticketStatus =="Open") {
-                sts.innerHTML = `<label class="control-label" style="padding-right: 0px;" for="sts">Status</label> <br> 
-                <span id="sts" class="indicator green"><span class="hidden-xs">${ticketStatus}</span></span>`
-            } 
-            else if (ticketStatus == "Completed"){
-                sts.innerHTML = `<label class="control-label" style="padding-right: 0px;" for="sts">Status</label> <br> 
-                <span id="sts" class="indicator grey"><span class="hidden-xs">${ticketStatus}</span></span>`
-                for(let field in fields){
-                    document.querySelector(`[data-fieldtype][data-fieldname="${fields[field]}"]`).setAttribute("disabled", "1")
-                }
+    let sts = document.querySelector('div.control-value');
+        // if save button clicked and the status is Completed then set fields as Read Only (Client side).
+        if (sts.innerHTML == "Completed"){
+            // set the required fields to read only
+            for (field in watchFields){
+                let targetField = document.querySelector(`[autocomplete][data-fieldtype][data-fieldname="${watchFields[field]}"]`);
+                // save the original value of the field
+                let tmpInnerHtml = targetField.value;
+                // inject frappe style of Read Only and restore the original fields values.
+                targetField.parentElement.innerHTML = `<div class="control-input-wrapper">
+                    <div class="control-input" style="display: none;"></div>
+                    <div class="control-value like-disabled-input" style="">${tmpInnerHtml}</div>
+                    <p class="help-box small text-muted hidden-xs"></p>
+                </div>`
             }
-        }
-    });
-
+        }        
     // check if submitted to the server and show message
     setTimeout(isSubmitted, 1000);
 }
@@ -60,7 +56,7 @@ frappe.ready(function() {
     // bind events here
 
     // give some time for the web form fields to be loaded.
-    setTimeout(set_props, 800);
+    setTimeout(set_props, 500);
 
         // hook the upper save button
     document.getElementsByClassName("btn-primary")[0].addEventListener("click", function(e) {
